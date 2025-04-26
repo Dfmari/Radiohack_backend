9c236943568f624864ae352f30cc21c9185c0877
Dfmari <damir.riyatov@gmail.com>
2025-04-27 04:50:06 +0500

    made test /start command
    Signed-off-by: Dfmari <damir.riyatov@gmail.com>

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import os
 import telebot
-
-# Try to load from .env (for local development)
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Get token from environment (works in GitHub Actions too)
-BOT_TOKEN = os.getenv("API_TOKEN")
+BOT_TOKEN = os.getenv("BOT_API_TOKEN")
 
 if not BOT_TOKEN:
     raise ValueError("❌ No TELEGRAM_BOT_TOKEN found! Check .env or GitHub Secrets.")
@@ -15,4 +14,6 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message, "🔍 Привет детектив 🔍 \n.... Надо сделать приветствие")
+    bot.send_message(message.chat.id, "🔍 Привет детектив 🔍 \n.... Надо сделать приветствие")
+
+bot.infinity_polling()
