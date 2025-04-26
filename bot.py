93d50824486f43d5edac43df286d2ce3f65fa415
Dfmari <damir.riyatov@gmail.com>
2025-04-27 04:38:42 +0500

    gitignore test

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import os
 import telebot
+
+# Try to load from .env (for local development)
+from dotenv import load_dotenv
+load_dotenv()
+
+# Get token from environment (works in GitHub Actions too)
+BOT_TOKEN = os.getenv("API_TOKEN")
+
+if not BOT_TOKEN:
+    raise ValueError("❌ No TELEGRAM_BOT_TOKEN found! Check .env or GitHub Secrets.")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message, "🔍 Привет детектив 🔍 \n.... Надо сделать приветствие")
