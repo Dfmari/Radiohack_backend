7a83a6fc721d88ddbec29bd2d0bbadf5ab7033d4
Dfmari <damir.riyatov@gmail.com>
2025-04-27 06:46:20 +0500

    made /help command
    Signed-off-by: Dfmari <damir.riyatov@gmail.com>

--- bot.py
+++ bot.py
@@ -8,12 +8,24 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_API_TOKEN")
 
 if not BOT_TOKEN:
-    raise ValueError("❌ No TELEGRAM_BOT_TOKEN found! Check .env or GitHub Secrets.")
+    raise ValueError("❌ No BOT_API_TOKEN found! Check .env or GitHub Secrets.")
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+bot.set_my_commands([
+    telebot.types.BotCommand("/start", "Приветствие"),
+    telebot.types.BotCommand("/help", "Команды"),
+    telebot.types.BotCommand("/play", "Играть"),
+    telebot.types.BotCommand("/top", "Лидерборд"),
+    telebot.types.BotCommand("/me", "Ваш рейтинг"),
+    telebot.types.BotCommand("/debug", "Не забудь удалить ;)"),
+])
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "🔍 Привет детектив 🔍 \n.... Надо сделать приветствие")
 
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "📚 Команды Бота 📚 \n ℹ️ /help Показывает этот список команд \n 🎮 /play Даёт вам ссылку на игру \n 🏆 /top Посмотрите на лидеров по рейтингу \n 😎 /me Посмотрите на каком месте вы в рейтинге \n 🛠️ /debug Не забудь удалить ;)")
 bot.infinity_polling()
